14e0418762f327cb0d37875d8708c420ae5c50bc
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-04-13 00:48:58 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ const Home = () => {
       <Button className="bg-primary">Button</Button>
       <h1>Hello World</h1>
       <span>The Title today</span>
+      <h1 className="text-center p-3 text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
