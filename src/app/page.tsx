37fa0cc3a33d1a97cea27b2d944b8e7ed273c6c5
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-04-14 20:26:08 +0200

    create login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import Login from "./(auth)/login/page";
 
 const Home = () => {
   return (
     <div>
-      <Button className="bg-primary">Button</Button>
-      <h1>Hello World</h1>
-      <span>The Title today</span>
-      <h1 className="text-center p-3 text-3xl font-bold underline">Hello world!</h1>
+      <Login />
     </div>
   );
 };
